c6cf622396d229f62b804c297a61fa86d11283c6
Andrew Stephan <thekiteeatingtree@gmail.com>
2014-01-06 17:09:36 -0500

    First commit

---
+++ src/Android/com/synconset/InternalStorage/InternalStorage.java
@@ -0,0 +1,64 @@
+/**
+ * An Internal Storage Plugin for Cordova/PhoneGap.
+ */
+package com.synconset;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
+
+public class InternalStorage extends CordovaPlugin {
+
+	public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
+		if (action.equals("getDir")) {
+			cordova.getThreadPool().execute(new Runnable() {
+				public void run() {
+					try {
+						JSONObject obj = requestFileSystem();
+						callbackContext.success(obj);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		} else {
+			return false;
+		}
+		return true;
+	}
+	
+	private JSONObject requestFileSystem() throws IOException, JSONException {
+		JSONObject fs = new JSONObject();
+		fs.put("name", "internal");
+		fs.put("root", getEntry(cordova.getActivity().getFilesDir()));
+		//fs.put("root", getEntry("/data/data/" + cordova.getActivity().getPackageName()));
+
+		return fs;
+	}
+	
+	/**
+     * Returns a JSON object representing the given File.
+     *
+     * @param file the File to convert
+     * @return a JSON representation of the given File
+     * @throws JSONException
+     */
+    public static JSONObject getEntry(File file) throws JSONException {
+        JSONObject entry = new JSONObject();
+
+        entry.put("isFile", file.isFile());
+        entry.put("isDirectory", file.isDirectory());
+        entry.put("name", file.getName());
+        entry.put("fullPath", "file://" + file.getAbsolutePath());
+        // The file system can't be specified, as it would lead to an infinite loop.
+        // entry.put("filesystem", null);
+
+        return entry;
+    }
+}
---
+++ www/internalstorage.js
@@ -0,0 +1,30 @@
+/*
+ * An Internal Storage Plugin for PhoneGap.
+*/
+
+//var FileError = require('./FileError');
+var FileSystem = require('org.apache.cordova.file.FileSystem');
+var exec = require('cordova/exec');
+
+var internalStorage = {};
+
+internalStorage.getDir = function(successCallback, errorCallback) {
+	var fail = function() {
+		errorCallback && errorCallback();	
+	};
+	var success = function(file_system) {
+		if (file_system) {
+			if (successCallback) {
+				// grab the name and root from the file system object
+				var result = new FileSystem(file_system.name, file_system.root);
+				successCallback(result);
+			}
+		} else {
+			// no FileSystem object returned
+			fail();
+		}
+	};
+  return exec(success, fail, "InternalStorage", "getDir", []);
+};
+
+window.internalStorage = internalStorage;
